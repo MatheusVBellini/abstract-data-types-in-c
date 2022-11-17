8a29eedc0afcdef51305bc179df07fe476e65241
Matheus Violaro Bellini <matheus.bellini02@gmail.com>
2022-11-16 23:21:38 -0300

    adaptations made

--- AVLTree.c
+++ AVLTree.c
@@ -15,48 +15,44 @@ elem max(elem a, elem b) {
     return (a > b) ? a : b;
 }
 
-int height(Node *root) {
-    if (root == NULL) {
+int height(Node *this) {
+    if (this == NULL) {
         return 0;
     }
-    return root->height;
+    return this->height;
 }
 
-int is_balanced(Node* root)
-{
-    if (root == NULL)
-        return 0;
-    int lh = is_balanced(root->left);
-    if (lh == -1)
-        return -1;
-    int rh = is_balanced(root->right);
-    if (rh == -1)
-        return -1;
- 
-    if (abs(lh - rh) > 1)
-        return -1;
-    else
-        return max(lh, rh) + 1;
-}
-
-int get_balance(Node *root) {
-    if (root == NULL) {
+int get_balance(Node *this) {
+    if (this == NULL) {
         return 0;
     }
-    return height(root->left) - height(root->right);
+
+    return height(this->left) - height(this->right);
 }
 
-Node *min_value_node(Node *root) {
-    Node *current = root;
+int is_balanced(Node *this) {
+	return (abs(get_balance(this)) >= 1) ? 1 : 0;
+}
+
+Node *next_in_order(Node *this) {
+    Node *current = this;
     while (current->left != NULL) {
         current = current->left;
     }
     return current;
 }
 
+Node *previous_in_order(Node *this) {
+	Node *current = this;
+	while (current->right != NULL) {
+		current = current->right;
+	}
+	return current;
+}
+
 // constructor
 Node *new_node(elem x) {
-    Node *new = (Node *) malloc(sizeof(Node));
+    Node *new = (Node *)malloc(sizeof(Node));
     new->x = x;
     new->left = NULL;
     new->right = NULL;
@@ -66,129 +62,128 @@ Node *new_node(elem x) {
 }
 
 // AVL rotations
-Node *right_rotate(Node *root) {
-    Node *x = root->left;
+Node *right_rotate(Node *this) {
+    Node *x = this->left;
     Node *y = x->right;
 
-    x->right = root;
-    root->left = y;
+    x->right = this;
+    this->left = y;
 
-    root->height = max(height(root->left), height(root->right)) + 1;
+    this->height = max(height(this->left), height(this->right)) + 1;
     x->height = max(height(x->left), height(x->right)) + 1;
 
     return x;
 }
 
-Node *left_rotate(Node *root) {
-    Node *x = root->right;
+Node *left_rotate(Node *this) {
+    Node *x = this->right;
     Node *y = x->left;
 
-    x->left = root;
-    root->right = y;
+    x->left = this;
+    this->right = y;
 
-    root->height = max(height(root->left), height(root->right)) + 1;
+    this->height = max(height(this->left), height(this->right)) + 1;
     x->height = max(height(x->left), height(x->right)) + 1;
 
     return x;
 }
 
 // insertion
-Node *insert(Node *root, elem x) {
+Node *insert(Node *this, elem x) {
     // binary search tree insertion
-    if (root == NULL) {
+    if (this == NULL) {
         return new_node(x);
     }
 
-    if (x < root->x) {
-        root->left = insert(root->left, x);
-    } else if (x > root->x) {
-        root->right = insert(root->right, x);
+    if (x < this->x) {
+        this->left = insert(this->left, x);
+    } else if (x > this->x) {
+        this->right = insert(this->right, x);
     } else {
-        return root;
+        return this;
     }
 
-    root->height = 1 + max(height(root->left), height(root->right));
+    this->height = max(height(this->left), height(this->right)) + 1;
 
     // check balance
-    int balance = get_balance(root);
+    int balance = get_balance(this);
 
-    if (balance > 1 && x < root->left->x) {
-        return right_rotate(root);
+    if (balance > 1) {
+        if (x > this->left->x)
+			this->left = left_rotate(this->left);
+		return right_rotate(this);
     }
 
-    if (balance < -1 && x > root->right->x) {
-        return left_rotate(root);
+    if (balance < -1) {
+		if (x < this->right->x) {
+			this->right = right_rotate(this->right);
+		}
+        return left_rotate(this);
     }
 
-    if (balance > 1 && x > root->left->x) {
-        root->left = left_rotate(root->left);
-        return right_rotate(root);
-    }
-
-    if (balance < -1 && x < root->right->x) {
-        root->right = right_rotate(root->right);
-        return left_rotate(root);
-    }
-
-    return root;
+    return this;
 }
 
 
 // removal 
-Node *remove_node(Node *root, elem x) {
-    if (root == NULL) {
-        return root;
+Node *remove_node(Node *this, elem x) {
+    if (this == NULL) {
+        return this;
     }
-
-    if (x < root->x) {
-        root->left = remove_node(root->left, x);
-    } else if (x > root->x) {
-        root->right = remove_node(root->right, x);
-    } else {
-        if ((root->left == NULL) || (root->right == NULL)) {
-            Node *tmp = (root->left) ? root->left : root->right;
+	
+    if (x < this->x) {									// x smaller, go left
+        this->left = remove_node(this->left, x);
+    } else if (x > this->x) {							// x greater, go right
+        this->right = remove_node(this->right, x);
+    } else {											// x equal, remove
+        if (this->left == NULL) {						// substitute with right node
+            Node *tmp = this->right;
 
             if (tmp == NULL) {
-                tmp = root;
-                root = NULL;
+                tmp = this;
+                this = NULL;
             } else {
-                *root = *tmp;
+                *this = *tmp;
             }
             free(tmp);
-        } else {
-            Node *tmp = min_value_node(root->right);
-            root->x = tmp->x;
-            root->right = remove_node(root->right, tmp->x);
+        } else if (this->right == NULL) {				// substitute with left node
+            Node *tmp = this->left;
+
+            if (tmp == NULL) {
+                tmp = this;
+                this = NULL;
+            } else {
+                *this = *tmp;
+            }
+            free(tmp);
+		} else {										// substitute with next in order
+            Node *tmp = next_in_order(this->right);
+            this->x = tmp->x;
+            this->right = remove_node(this->right, tmp->x);
         }
     }
 
-    if (root == NULL) {
-        return root;
+    if (this == NULL) {
+        return this;
     }
 
-    root->height = 1 + max(height(root->left), height(root->right));
+    this->height = max(height(this->left), height(this->right)) + 1;
 
-    int balance = get_balance(root);
+    int balance = get_balance(this);
 
-    if (balance > 1 && get_balance(root->left) >= 0) {
-        return right_rotate(root);
+    if (balance > 1) {
+		if (get_balance(this->left) < 0)
+	        this->left = left_rotate(this->left);
+        return right_rotate(this);
     }
 
-    if (balance > 1 && get_balance(root->left) < 0) {
-        root->left = left_rotate(root->left);
-        return right_rotate(root);
+    if (balance < -1) {
+		if (get_balance(this->right) > 0)
+			this->right = right_rotate(this->right);
+        return left_rotate(this);
     }
 
-    if (balance < -1 && get_balance(root->right) <= 0) {
-        return left_rotate(root);
-    }
-
-    if (balance < -1 && get_balance(root->right) > 0) {
-        root->right = right_rotate(root->right);
-        return left_rotate(root);
-    }
-
-    return root;
+    return this;
 }
 
 // traversal
@@ -237,19 +232,41 @@ void post_order_traversal(Node *node) {
 }
 
 int main() {
-    Node *root = NULL;
+    Node *this = NULL;
 
-    root = insert(root, 10);
-    root = insert(root, 20);
-    root = insert(root, 30);
-    root = insert(root, 40);
-    root = insert(root, 50);
-    root = insert(root, 25);
+    this = insert(this, 4);
+    this = insert(this, 3);
+    this = insert(this, 1);
+    this = insert(this, 10);
+    this = insert(this, 11);
+    this = insert(this, 0);
+    this = insert(this, 2);
+    this = insert(this, 5);
 
-    root = remove_node(root, 20);
+    level_order_traversal(this);
 
-    pre_order_traversal(root);
-    printf("\n\n%d\n\n", is_balanced(root));
+	printf("\n\n\n");
+
+	remove_node(this,4);
+    level_order_traversal(this);
+	printf("\n\n\n");
+
+	remove_node(this,3);
+    level_order_traversal(this);
+	printf("\n\n\n");
+
+	remove_node(this,7);
+    level_order_traversal(this);
+	printf("\n\n\n");
+
+	remove_node(this,1);
+    level_order_traversal(this);
+	printf("\n\n\n");
+
+	remove_node(this,2);
+    level_order_traversal(this);
+	printf("\n\n\n");
+
 
     return 0;
 }
